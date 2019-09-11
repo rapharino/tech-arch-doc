3223e420a83184c1ea0030d564eabcde66ac7983
realpdai <suzhou.daipeng@gmail.com>
2019-09-11 14:55:59 +0800

    updated

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
         "/": {
             lang: "zh-CN",
             title: "戴鹏的个人博客",
-            description: "包含：Java 基础, Java 部分源码, JVM, Spring, Spring Boot, Spring Cloud, 数据库原理, MySQL, ElasticSearch, MongoDB, Docker, k8s, CI&CD, Linux, DevOps, 分布式, 中间件, 开发工具, Git, IDE, 源码阅读，读书笔记, 开源项目..."
+            description: "Java 基础, Java 部分源码, JVM, Spring, Spring Boot, Spring Cloud, 数据库原理, MySQL, ElasticSearch, MongoDB, Docker, k8s, CI&CD, Linux, DevOps, 分布式, 中间件, 开发工具, Git, IDE, 源码阅读，读书笔记, 开源项目..."
         }
     },
     head: [["link", {rel: "icon", href: `/favicon.ico`}]],
